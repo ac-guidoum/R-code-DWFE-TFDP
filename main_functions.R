2cdbfffc7475a570271e895cd76004922a1fe5c2
ac-guidoum <starsalane@gmail.com>
2026-04-18 22:44:34 +0100

    main_functions.R

--- main_functions.R
+++ main_functions.R
@@ -5,25 +5,6 @@
 options(future.globals.maxSize = +Inf, warn = -1, digits = 5)
 
 #================================================================
-# load R packages
-#================================================================
-
-if (!require("Sim.DiffProc")) { install.packages("Sim.DiffProc"); library("Sim.DiffProc") }
-if (!require("yuima"))        { install.packages("yuima");        library("yuima")        }
-if (!require("ttutils"))      { install.packages("ttutils");      library("ttutils")      }
-if (!require("EQL"))          { install.packages("EQL");          library("EQL")          }
-if (!require("gsl"))          { install.packages("gsl");          library("gsl")          }
-if (!require("ggsci"))        { install.packages("ggsci");        library("ggsci")        }
-if (!require("furrr"))        { install.packages("furrr");        library("furrr")        }
-if (!require("Deriv"))        { install.packages("Deriv");        library("Deriv")        }
-if (!require("zoo"))          { install.packages("zoo");          library("zoo")          }
-if (!require("ggplot2"))      { install.packages("ggplot2");      library("ggplot2")      }
-if (!require("tidyr"))        { install.packages("tidyr");        library("tidyr")        }
-if (!require("dplyr"))        { install.packages("dplyr");        library("dplyr")        }
-if (!require("scales"))       { install.packages("scales");       library("scales")       }
-if (!require("MASS"))         { install.packages("MASS");         library("MASS")         }
-
-#================================================================
 # Simulation model Eq.(1)
 #================================================================
 
@@ -89,7 +70,7 @@ HO_mom <- function(x0, gt, k = NULL, Alpha = 1, Beta = 1, Gamma = 1,
                    H = 0.75, p = 1, t) {
   
   if (!inherits(gt, "expression")) stop("'gt' must be an expression in 't'")
-  if (H < 0.5 | H >= 1) stop("'H' must be in '(1/2,1)'")
+  if (H < 0.5 | H >= 1)  stop("'H' must be in '(1/2,1)'")
   if (p <= 0)            stop("'p' must be numeric > 0'")
   if (Gamma <= 0)        stop("'Gamma' must be numeric > 0'")
   
@@ -205,6 +186,7 @@ WFE_theor <- function(x0, gt, k = NULL, Alpha = 1, Beta = 1,
     result[["cor3"]] <- HS_vec * Ep_vec +
       (p * (p - 1) / 2) * s2_vec * Ep_2_vec
   }
+  
   return(invisible(result))
 }
 
@@ -270,7 +252,8 @@ WFE_emp <- function(X = NULL, x0, gt, k = NULL, Alpha = 1, Beta = 1,
     }, numeric(1))
     
     if (any(is.na(WFE_t)))
-      WFE_t <- zoo::na.approx(WFE_t, na.rm = FALSE)   
+      WFE_t <- zoo::na.approx(WFE_t, na.rm = FALSE)
+    
   } else { 
     WFE_t <- vapply(temps_pos, function(ti) {
       mu_i  <- mu_X(ti)
@@ -280,6 +263,7 @@ WFE_emp <- function(X = NULL, x0, gt, k = NULL, Alpha = 1, Beta = 1,
       p_hat <- pmax(p_hat, .Machine$double.eps)
       mean(x_i^p * (-log(p_hat)))
     }, numeric(1))
-  } 
+  }
+  
   return(invisible(WFE_t))
 }
